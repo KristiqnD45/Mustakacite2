ba555f4bcff39ab7f0bb7f110b30ce679b1619eb
KristiqnD45 <136679017+KristiqnD45@users.noreply.github.com>
2026-03-18 10:36:21 +0200

    fix

--- obj/Debug/net9.0/ASPMustakacite.AssemblyInfo.cs
+++ obj/Debug/net9.0/ASPMustakacite.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPMustakacite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88e0a7fdf25c2b8eeaba87ff244c9d799abb80b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPMustakacite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPMustakacite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
